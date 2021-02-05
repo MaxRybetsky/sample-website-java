17f5986fa7a18fc271a061d87bb2526475a11acf
MaxRybetsky <stsdewar@ya.ru>
2021-02-05 22:04:55 +0300

    Second step: Add Database to Web application

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,13 +1,28 @@
 package com.example.servingwebcontent;
 
+import com.example.servingwebcontent.domain.Message;
+import com.example.servingwebcontent.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    private MessageRepo messageRepo;
+
+    public GreetingController() {
+    }
+
+    @Autowired
+    public GreetingController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false,
             defaultValue = "World") String name, Map<String, Object> model) {
@@ -17,7 +32,29 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hello at main");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String newMessage(@RequestParam String text, @RequestParam String tag,
+                             Map<String, Object> model) {
+        messageRepo.save(new Message(text, tag));
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String doFilter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages = null;
+        if (filter == null || "".equals(filter)) {
+            messages = messageRepo.findAll();
+        } else {
+            messages = messageRepo.findByTag(filter);
+        }
+        model.put("messages", messages);
         return "main";
     }
 }
